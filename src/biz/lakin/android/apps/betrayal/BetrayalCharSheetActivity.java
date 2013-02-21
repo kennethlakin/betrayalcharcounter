55895a54e79eafd9b10982f8694ade6932d41b68
Kenneth Lakin <kennethlakin@gmail.com>
2013-02-20 23:13:32 -0800

    * Initial cut at character stats. Not sure if this is best approach.

--- src/biz/lakin/android/apps/betrayal/BetrayalCharSheetActivity.java
+++ src/biz/lakin/android/apps/betrayal/BetrayalCharSheetActivity.java
@@ -1,14 +1,96 @@
 package biz.lakin.android.apps.betrayal;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 
 public class BetrayalCharSheetActivity extends Activity {
+	
+	private final int maxStatIndex = 7;
+	private final int maxStatNameIndex = 3;
+	private String[] names = {
+			"Heather", "Jenny",
+			"Peter", "Brandon",
+			"Professor", "Father",
+			"Vivian", "Madame",
+			"Ox", "Darrin",
+			"Zoe", "Missy"
+	};
+	private String[] statNames = { "Speed", "Might", "Sanity", "Knowledge" };
+	private int[][][] charStats = {
+			{ //Heather
+				{3,3,4,5,6,6,7,8},
+				{3,3,3,4,5,6,7,8},
+				{3,3,3,4,5,6,6,6},
+				{2,3,3,4,5,6,7,8}
+			},
+			{ //Jenny
+				{2,3,4,4,4,5,6,8},
+				{3,4,4,4,4,5,6,8},
+				{1,1,2,4,4,4,5,6},
+				{2,3,3,4,4,5,6,8}
+			},
+			{ //Peter
+				{3,3,3,4,6,6,7,7},
+				{2,3,3,4,5,5,6,8},
+				{3,4,4,4,5,6,6,7},
+				{3,4,4,5,6,7,7,8}
+			},
+			{ //Brandon
+				{3,4,4,4,5,6,7,8},
+				{2,3,3,4,5,6,6,7},
+				{3,3,3,4,5,6,7,8},
+				{1,3,3,5,5,6,6,7}
+			},
+			{ //Professor
+				{2,2,4,4,5,5,6,6},
+				{1,2,3,4,5,5,6,6},
+				{1,3,3,4,5,5,6,7},
+				{4,5,5,5,5,6,7,8}
+			},
+			{ //Father
+				{2,3,3,4,5,6,7,7},
+				{1,2,2,4,4,5,5,7},
+				{3,4,5,5,6,7,7,8},
+				{1,3,3,4,5,6,6,8}
+			},
+			{ //Viv
+				{3,4,4,4,4,6,7,8},
+				{2,2,2,4,4,5,6,6},
+				{4,4,4,5,6,7,8,8},
+				{4,5,5,5,5,6,6,7}
+			},
+			{ //Madame
+				{2,3,3,5,5,6,6,7},
+				{2,3,3,4,5,5,5,6},
+				{4,4,4,5,6,7,8,8},
+				{1,3,4,4,4,5,6,6}
+			},
+			{ //Ox
+				{2,2,2,3,4,5,5,6},
+				{4,5,5,6,6,7,8,8},
+				{2,2,3,4,5,5,6,7},
+				{2,2,3,3,5,5,6,6}
+			},
+			{ //Darrin
+				{4,4,4,5,6,7,7,8},
+				{2,3,3,4,5,6,6,7},
+				{1,2,3,4,5,5,5,7},
+				{2,3,3,4,5,5,5,7}
+			},
+			{ //Zoe
+				{4,4,4,4,5,6,8,8},
+				{2,2,3,3,4,4,6,7},
+				{3,4,5,5,6,6,7,8},
+				{1,2,3,4,4,5,5,5}
+			},
+			{ //Missy
+				{3,4,5,6,6,6,7,7},
+				{2,3,3,3,4,5,6,7},
+				{1,2,3,4,5,5,6,7},
+				{2,3,4,4,5,6,6,6}
+			}
+	};
 
-	private ArrayList<View> speedButtons = new ArrayList<View>();
 	public BetrayalCharSheetActivity() {
 		
 	}
